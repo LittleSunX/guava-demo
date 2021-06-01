8642cedec4d57b93d53028eb06edaf5ddfdaeec9
LittleSunX <343395957@qq.com>
2021-06-01 15:15:04 +0800

    更新算法库

--- src/test/java/com/guava/test/LeetCodeTest.java
+++ src/test/java/com/guava/test/LeetCodeTest.java
@@ -47,12 +47,16 @@ public class LeetCodeTest {
         return new int[0];
     }
 
+
     /**
      * 两数相加
      * 给你两个非空的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，
      * 并且每个节点只能存储一位数字。
      * 请你将两个数相加，并以相同形式返回一个表示和的链表。
      * 你可以假设除了数字 0 之外，这两个数都不会以 0开头。
+     * 输入：l1 = [2,4,3], l2 = [5,6,4]
+     * 输出：[7,0,8]
+     * 解释：342 + 465 = 807.
      *
      * @param l1 链表1
      * @param l2 链表2
@@ -90,6 +94,10 @@ public class LeetCodeTest {
 
     /**
      * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+     * 示例 1:
+     * 输入: s = "abcabcbb"
+     * 输出: 3
+     * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
      *
      * @param s 给定的字符串
      * @return 返回最大不重复子串的长度
@@ -123,6 +131,19 @@ public class LeetCodeTest {
         }
 
         return maxLen;
+    }
+
+    /**
+     * 高斯算法
+     * 首项加末项乘以项数除以2
+     */
+    @Test
+    public void sum() {
+        int sum = 0;
+        int n = 100;
+        sum += (1 + n) * n / 2;
+        System.out.println(sum);
+        TestCase.assertEquals(sum, 5050);
     }
 
 
